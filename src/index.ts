75410bde8ae34a7ddd708076918dc147fd1a28f2
eduardo732 <eduardo.fuentesreyes@gmail.com>
2024-10-15 11:03:45 -0300

    feature: endpoints getters

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,42 @@ const port = 3000;
 
 app.use(express.json());
 
+// con npm install faker puedes usar faker para generar datos aleatorios
 app.get("/", (req: Request, res: Response) => {
   res.send({ message: "Hello World" });
+});
+
+app.get("/products", (req: Request, res: Response) => {
+  res.send([
+    {
+      id: 1,
+      name: "Product 1",
+      price: 100,
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      price: 200,
+    },
+  ]);
+});
+
+app.get("/products/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+  const products = [
+    {
+      id: 1,
+      name: "Product 1",
+      price: 100,
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      price: 200,
+    },
+  ];
+  const product = products.find((product) => product.id === parseInt(id));
+  res.send(product);
 });
 
 //Manejo de errores
